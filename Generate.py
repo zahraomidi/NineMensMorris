045b037911638072fc6c0302c905c2e81789fbd2
Zahra Omidi <zahra.omidi216@gmail.com>
2023-07-02 16:24:43 -0500

    modified Generate

--- Generate.py
+++ Generate.py
@@ -10,9 +10,39 @@ def GenerateAdd(board):
                 GenerateRemove(temp_board, L)
             else:
                 L.append(temp_board)
+    return L
 
+def GenerateHopping(board):
+    List = []
+    for i in range(0,21):
+        if board[i]=='W':
+            for j in range(0,21):
+                if board[j]=='x':
+                    temp_board = board
+                    board[i] = 'x'
+                    board[j] = 'W'
+                    if CloseMill(j, temp_board):
+                        GenerateRemove(temp_board, List=List)
+                    else:
+                        List.append(temp_board)
+    return List
 
-     
+def GenerateMove(board):
+    List = []
+    for i in range(0,21):
+        if board[i]=='W':
+            n = adjacent_locations(i)
+            for neighbor in n:
+                if board[neighbor]=='x':
+                    temp_board = board
+                    temp_board[i]='x'
+                    temp_board[neighbor]='W'
+                    if CloseMill(neighbor, temp_board):
+                        GenerateRemove(temp_board, List)
+                    else:
+                        List.append(temp_board)
+    return List
+   
 def GenerateRemove(board, List):
     change = False
     for i in range(0,21):
@@ -22,6 +52,5 @@ def GenerateRemove(board, List):
                 temp_board[i] = 'x'
                 List.append(temp_board)
                 change = True
-
     if change:
         List.append(board)
