36f606342d33e9b66035f503b380b4a63a3be88c
Zahra Omidi <zahra.omidi216@gmail.com>
2023-07-05 13:15:58 -0500

    -

--- MinMaxTreeNode.py
+++ MinMaxTreeNode.py
@@ -1,10 +1,40 @@
+from copy import deepcopy
+from Utilies import *
+from Generate import *
+
 class Node(object):
-	def __init__(self, board, blackTurn, parent=None, nextNodes=None):
+        
+	def __init__(self, board, blackTurn=False, parent=None, nextNodes=None):
 		if nextNodes == None:
 			nextNodes = []
 		self.board = board
-		self.blackToMove = blackTurn
+		self.blackTurn = blackTurn
+        if self.blackTurn:
+            self.board = Inverted_Board(self.board)
 		self.parent = parent
-		self.nextStates = nextNodes
-		
-    
+		self.nextNodes = nextNodes
+                
+    def make_children_opening(self):
+        for i in range(0,21):
+            if self.board[i] == 'x':
+                temp_board = deepcopy(self.board)
+                temp_board[i] = 'w'
+                if CloseMill(i, temp_board):
+                    self.nextNodes = GenerateRemove(temp_board, self.nextNodes)
+                else:
+                    if not self.blackTurn:
+                        self.nextNodes.append(Node(temp_board, True, self))
+                    else:
+                        self.nextNodes.append(Node(temp_board, False, self))
+
+    def GenerateRemove(self):
+        change = False
+        for i in range(0,21):
+            if self.board[i]=='B':
+                if not CloseMill(i, self.board):
+                    temp_board = deepcopy(self.board)
+                    temp_board[i] = 'x'
+                    self.nextNodes.append(temp_board)
+                    change = True
+        if not change:      # check this! --------------------------
+            self.nextNodes.append(board)
