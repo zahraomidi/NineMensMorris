405d99352d64004462f64f2d36e90a194aa74faf
Zahra Omidi <zahra.omidi216@gmail.com>
2023-07-05 15:16:43 -0500

    fixed some bugs for generating children

--- MinMaxTreeNode.py
+++ MinMaxTreeNode.py
@@ -1,4 +1,6 @@
 from copy import deepcopy
+# import Utilies as U
+# import Generate as G
 from Utilies import *
 from Generate import *
 
@@ -8,30 +10,32 @@ class Node(object):
             nextNodes = []
         self.board = board
         self.blackTurn = blackTurn
-        if self.blackTurn:
-            self.board = Inverted_Board(self.board)
+        # if self.blackTurn:
+        #     self.board = Inverted_Board(self.board)
+        #     print("inverted board")
         self.parent = parent
         self.nextNodes = nextNodes
                 
-    def make_children_opening(self):
+    def generate_opening_children(self):            #GenerateAdd function in handout
         for i in range(0,21):
             if self.board[i] == 'x':
                 temp_board = deepcopy(self.board)
-                temp_board[i] = 'w'
+                temp_board[i] = 'W'
                 if CloseMill(i, temp_board):
-                    self.GenerateRemove()
+                    self.GenerateRemove(temp_board)
+                    
                 else:
                     self.nextNodes.append(Node(temp_board, not self.blackTurn, self))
 
-    def GenerateRemove(self):
+    def GenerateRemove(self, board):
         change = False
         for i in range(0,21):
-            if self.board[i]=='B':
-                if not CloseMill(i, self.board):
-                    temp_board = deepcopy(self.board)
+            if board[i]=='B':
+                if not CloseMill(i, board):
+                    temp_board = deepcopy(board)
                     temp_board[i] = 'x'
                     self.nextNodes.append(Node(temp_board, not self.blackTurn, self))
                     change = True
 
-        if not change:      # check this! --------------------------
-            self.nextNodes.append(board)
+        if not change:     
+            self.nextNodes.append(Node(board, not self.blackTurn, self))
