278535bcdc785fb88767c9a3d2524de17e486aff
Zahra Omidi <zahra.omidi216@gmail.com>
2023-08-07 14:49:46 -0500

    -

---
+++ tournamentGame.py
@@ -0,0 +1,115 @@
+from Utilies import *
+from MinMaxTreeNode import Node
+import math
+import time
+
+global position_num
+
+w2 = open('ABtourGame_children.txt', 'w')
+
+def write_children(node):
+    w2.write(board_position_to_str(node.board))
+    w2.write('\t')
+    if not node.static == None:
+        w2.write(str(node.static))
+    w2.write('\n')
+    for child in node.children:
+        write_children(child)
+
+def traverse_tree(init_node, node_depth, tree_depth):
+    if node_depth < tree_depth:
+        if init_node.blackTurn:
+            init_node.MoveGeneratorBlack('midend')
+        else: 
+            init_node.GenerateMovesMidgameEndgame()
+        for child in init_node.children:
+            traverse_tree(child, node_depth+1, tree_depth)
+
+
+def MaxMin(init_node, alpha, betha):
+
+    global position_num
+    position_num += 1
+
+    if init_node.children == None or init_node.children == []:
+        init_node.static_estimation_improved()
+        return init_node.static
+    
+    v = -math.inf
+    for child in init_node.children:
+        v = max(v, MinMax(child, alpha, betha)) 
+        if v >= betha:
+            init_node.static = v
+            return v
+        else:
+            alpha = max(v, alpha)
+
+    init_node.static = v
+    return v
+
+def MinMax(init_node, alpha, betha):
+
+    global position_num
+    position_num += 1
+
+    if init_node.children == None or init_node.children == []:
+        init_node.static_estimation_improved()
+        return init_node.static
+        
+    
+    v = math.inf
+    for child in init_node.children:
+        v = min(v, MaxMin(child, alpha, betha))
+        if v <= alpha:
+            init_node.static = v
+            return v
+        else:
+            betha = min(v, betha)
+
+    init_node.static = v
+    return v
+
+if __name__ == '__main__':
+
+    # input_file = 'b1.txt'
+    output_file = 'AB_tour_Game.txt'
+    depth = 7
+
+    board = 'BBxWxBWBWWWWxxxxWBBWx'
+
+    # f = open(input_file, "r")
+    # board = f.read()
+    board = [item for item in board]
+    temp_board = Node(board=board)
+    temp_board.PrintBoard()
+
+    position_num = 0
+    
+    start = time.time()
+    traverse_tree(temp_board, 0, depth)
+
+    output_static = MaxMin(temp_board, -math.inf, math.inf)
+    end = time.time()
+
+    print(str((end-start)))
+    # write_children(temp_board)
+
+    Best_Move = temp_board
+    Best_static = -math.inf
+    for child in temp_board.children:
+        if child.static > Best_static:
+            Best_static = child.static
+            Best_Move = child
+
+    Best_Move.PrintBoard()
+    print(str(board_position_to_str(Best_Move.board)))
+    w = open(output_file, 'w')
+
+    w.write('Board Position:\t')
+    w.write(str(board_position_to_str(Best_Move.board)) + '\n')
+
+    w.write('Positions evaluated by static estimation:\t')
+    w.write(str(position_num)+'\n')
+
+    w.write('MINIMAX estimate:\t')
+    w.write(str(Best_Move.static))
